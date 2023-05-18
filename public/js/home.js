853399ceddf0a908779c740ace04c80ca78ce8c9
Piyush Prajapati <piyushoa2004@gmail.com>
2023-05-18 19:01:08 +0530

    18-may

--- public/js/home.js
+++ public/js/home.js
@@ -1,12 +1,2 @@
-let popupContainer = document.getElementById("contact-us-popup-container");
-let contactButton = document.getElementById("primary-action");
 
-let popupHideButton = document.getElementById("popup-hide-button");
 
-contactButton.addEventListener("click", function () {
-    popupContainer.style.display = "flex";
-})
-
-popupHideButton.addEventListener("click", function () {
-    popupContainer.style.display = "none";  
-})
