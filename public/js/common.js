6812609ba15345431fb8e2ef4e79132a1da2fd44
Piyush Prajapati <piyushoa2004@gmail.com>
2023-07-20 12:03:35 +0530

    20-july

--- public/js/common.js
+++ public/js/common.js
@@ -1,5 +1,6 @@
 let navButton = document.getElementById("nav-link-button");
 let navList = document.getElementById("nav-list");
+let bodyBackground = document.getElementById("background-container");
 
 navButton.addEventListener("click", function () {
 
@@ -8,10 +9,20 @@ navButton.addEventListener("click", function () {
     if (navListDisplay == 'none') {
         navList.style.display = "flex";
         navButton.style.backgroundColor = "#f1f1f1";
+        bodyBackground.style.display = "block";
     }
 
     else {
         navList.style.display = "none";
         navButton.style.backgroundColor = "#fff";
+        bodyBackground.style.display = "none";
     }
+});
+
+bodyBackground.addEventListener("click", () => {
+    if ( bodyBackground.style.display == "block" ) {
+        navList.style.display = "none";
+        navButton.style.backgroundColor = "#fff";
+        bodyBackground.style.display = "none";
+        }
 });
